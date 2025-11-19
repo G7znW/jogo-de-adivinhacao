155123ae8eb98e41860282545bd31090503766be
G7znW <gustafonso10@gmail.com>
2025-11-19 09:53:12 -0300

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'jogo_adivinhacao';
-$user = 'root';     // altere se necessário
-$pass = '';         // senha do seu MySQL (se houver)
+$host = 'sql100.infinityfree.com';
+$dbname = 'if0_&0¨&913%_jogo_adivinhacao';
+$user = 'if0_*0%$913@';     // altere se necessário
+$pass = '13270514';         // senha do seu MySQL (se houver)
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
@@ -10,3 +10,4 @@ try {
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
 }
+
